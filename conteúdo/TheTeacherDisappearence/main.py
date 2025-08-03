466ffd0a26d563e3e3fd087b7aa073b24e559b2e
diaslucas06 <dias.lucas1@escolar.ifrn.edu.br>
2025-08-03 17:10:16 -0300

    Importamos a classe Key1 do arquivo keys.py, ajustei o FPS em uma variável, adicionei o grupo items para os itens do jogo ficarem nele e adicionei a condição para o personagem agachar ao pressioner e perto do item

--- conteúdo/TheTeacherDisappearence/main.py
+++ conteúdo/TheTeacherDisappearence/main.py
@@ -1,16 +1,22 @@
 import pygame
 from pygame.locals import *
 from player import Player
+from items.keys import Key1
 import os
 
 pygame.init()
 
 LARGURA = 1280
 ALTURA = 720
+FPS = 30
 
 player_andar = pygame.sprite.Group()
 player = Player()
 player_andar.add(player)
+
+items = pygame.sprite.Group()
+chave = Key1()
+items.add(chave)
 
 tela = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption("The Teacher Disappearence")
@@ -20,17 +26,26 @@ fundo = pygame.transform.scale(fundo, (LARGURA, ALTURA))
 relogio = pygame.time.Clock()
 
 while True:
-    relogio.tick(30)
-    tela.blit(fundo, (0,0))
-
+    
+    relogio.tick(FPS)
+    teclas = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-        if event.type == KEYDOWN:
+        elif event.type == KEYDOWN:
             player.andando()
-        
-    teclas = pygame.key.get_pressed()
-    player_andar.draw(tela)
+            
+    for item in items:
+        if player.rect.colliderect(item.rect) and not item.coletado:
+            if teclas[pygame.K_e]:
+                player.coletando()
+                item.coletado = True
+                    
+    items.update()            
     player_andar.update(teclas)
+    
+    tela.blit(fundo, (0,0))
+    items.draw(tela)
+    player_andar.draw(tela)
     pygame.display.flip()
