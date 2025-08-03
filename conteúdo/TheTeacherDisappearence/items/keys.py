ce80e67f6146999cc6bf35ad03c7294044f04ec4
diaslucas06 <dias.lucas1@escolar.ifrn.edu.br>
2025-08-03 17:07:45 -0300

    Criamos a classe Key1 para definir um objeto simples, que é a chave na tela, e para quando ela for coletada, ela desaparecer

--- conteúdo/TheTeacherDisappearence/items/keys.py
+++ conteúdo/TheTeacherDisappearence/items/keys.py
@@ -1,1 +1,20 @@
- 
+import pygame 
+import os
+
+class Key1(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(os.path.dirname(__file__),"..", "data", "images", "items", f"key.png"))
+        self.image = pygame.transform.scale(self.image, (30, 50))
+        
+        self.rect = self.image.get_rect()
+        self.rect.topleft = 500, 520
+        
+        self.coletado = False
+        
+    def update(self):
+        if self.coletado:
+            self.kill()
+            
