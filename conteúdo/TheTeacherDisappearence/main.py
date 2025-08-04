5773e1a60fd7e2f826354f9420bcac0dd35c0211
diaslucas06 <dias.lucas1@escolar.ifrn.edu.br>
2025-08-04 11:42:41 -0300

    Criei a classe Game e adicionei as informações do main nela

--- conteúdo/TheTeacherDisappearence/main.py
+++ conteúdo/TheTeacherDisappearence/main.py
@@ -10,42 +10,48 @@ LARGURA = 1280
 ALTURA = 720
 FPS = 30
 
-player_andar = pygame.sprite.Group()
-player = Player()
-player_andar.add(player)
+class Game():
+    def __init__(self):
+        self.player_andar = pygame.sprite.Group()
+        self.player = Player()
+        self.player_andar.add(self.player)
 
-items = pygame.sprite.Group()
-chave = Key1()
-items.add(chave)
+        self.items = pygame.sprite.Group()
+        self.chave = Key1()
+        self.items.add(self.chave)
 
-tela = pygame.display.set_mode((LARGURA, ALTURA))
-pygame.display.set_caption("The Teacher Disappearence")
-caminho = os.path.join(os.path.dirname(__file__), "data", "images", "corredores", "CorredorA36.png")
-fundo = pygame.image.load(caminho).convert()
-fundo = pygame.transform.scale(fundo, (LARGURA, ALTURA))
-relogio = pygame.time.Clock()
+        self.tela = pygame.display.set_mode((LARGURA, ALTURA))
+        pygame.display.set_caption("The Teacher Disappearence")
+        self.caminho = os.path.join(os.path.dirname(__file__), "data", "images", "corredores", "CorredorA36.png")
+        self.fundo = pygame.image.load(self.caminho).convert()
+        self.fundo = pygame.transform.scale(self.fundo, (LARGURA, ALTURA))
+        self.relogio = pygame.time.Clock()
 
-while True:
+    def run(self):
     
-    relogio.tick(FPS)
-    teclas = pygame.key.get_pressed()
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            exit()
-        elif event.type == KEYDOWN:
-            player.andando()
+        while True:
             
-    for item in items:
-        if player.rect.colliderect(item.rect) and not item.coletado:
-            if teclas[pygame.K_e]:
-                player.coletando()
-                item.coletado = True
+            self.relogio.tick(FPS)
+            
+            self.teclas = pygame.key.get_pressed()
+            
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    exit()
+                elif event.type == KEYDOWN:
+                    self.player.andando()
                     
-    items.update()            
-    player_andar.update(teclas)
-    
-    tela.blit(fundo, (0,0))
-    items.draw(tela)
-    player_andar.draw(tela)
-    pygame.display.flip()
+            for item in self.items:
+                if self.player.rect.colliderect(item.rect) and not item.coletado:
+                    if self.teclas[pygame.K_e]:
+                        self.player.coletando()
+                        item.coletado = True
+                            
+            self.items.update()            
+            self.player_andar.update(self.teclas)
+            
+            self.tela.blit(self.fundo, (0,0))
+            self.items.draw(self.tela)
+            self.player_andar.draw(self.tela)
+            pygame.display.flip()
