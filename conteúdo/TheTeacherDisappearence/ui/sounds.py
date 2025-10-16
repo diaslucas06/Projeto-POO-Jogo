62bf7b605206d5f608cfc21ef98c9f7b1bf1fd93
Lucas Gabriel de Araújo Dias <dias.lucas1@escolar.ifrn.edu.br>
2025-10-16 11:12:11 -0300

    Classes para as músicas e os sons tocaram.

--- conteúdo/TheTeacherDisappearence/ui/sounds.py
+++ conteúdo/TheTeacherDisappearence/ui/sounds.py
@@ -1,1 +1,24 @@
- 
+import pygame
+import os
+
+class Musica():
+    def __init__(self, arquivo):
+        
+        self.caminho = os.path.join(os.path.dirname(__file__), "..", "data", "sounds", arquivo)
+        
+    def play(self):
+        pygame.mixer.music.set_volume(0.3)
+        pygame.mixer.music.load(self.caminho)
+        pygame.mixer.music.play(-1)
+        
+    def parar(self):
+        pygame.mixer.music.stop()
+        
+class Som():
+    def __init__(self, som):
+        self.caminho = os.path.join(os.path.dirname(__file__), "..", "data", "sounds", som)
+        self.som = pygame.mixer.Sound(self.caminho)
+        
+    def play(self):
+        self.som.play()
+        
