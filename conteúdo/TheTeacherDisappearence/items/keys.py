a677615b93e11ec89e661694c4727270d60a6e70
Lucas Gabriel de Araújo Dias <dias.lucas1@escolar.ifrn.edu.br>
2025-10-16 11:32:37 -0300

    Ajustei alguns problemas nos itens, como estar passadno o inventário direto no arquivo e a ordem dentro dos __init__.

--- conteúdo/TheTeacherDisappearence/items/keys.py
+++ conteúdo/TheTeacherDisappearence/items/keys.py
@@ -1,21 +1,17 @@
 import pygame 
 import os
-from ui.hud import Inventario
-
-inventario = Inventario()
 
 class Item(pygame.sprite.Sprite):
     
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.caminho = os.path.join(os.path.dirname(__file__),"..", "data", "images", "items", "key.png")
         self.image = pygame.image.load(self.caminho)
         self.image = pygame.transform.scale(self.image, (25, 40))
         self.rect = self.image.get_rect()
         self.rect.topleft = x, y
         self.coletado = False
         
-    def update(self):
+    def update(self, inventario):
         if self.coletado:
             inventario.i = inventario.i + 50
             self.rect.topleft = inventario.i, 635
@@ -24,13 +20,18 @@ class Item(pygame.sprite.Sprite):
 class Key1(Item):
 
     def __init__(self, x, y):
-        super().__init__(x, y)
         self.caminho = os.path.join(os.path.dirname(__file__),"..", "data", "images", "items", "key.png")
+        super().__init__(x, y)
         
+class Key2(Item):
 
+    def __init__(self, x, y):
+        self.caminho = os.path.join(os.path.dirname(__file__),"..", "data", "images", "items", "key.png")
+        super().__init__(x, y)
+        
 class Fita(Item):
     
     def __init__(self, x, y):
-        super().__init__(x, y)
         self.caminho = os.path.join(os.path.dirname(__file__),"..", "data", "images", "items", "Itens_do_jogador.png")
+        super().__init__(x, y)
         
