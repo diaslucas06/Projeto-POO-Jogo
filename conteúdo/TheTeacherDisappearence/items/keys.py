04cdb4dcb789e5f0dce4f1af4376e6000f7b0497
Lucas Gabriel de Araújo Dias <dias.lucas1@escolar.ifrn.edu.br>
2025-10-16 11:13:52 -0300

    Correções nas classes dos itens.

--- conteúdo/TheTeacherDisappearence/items/keys.py
+++ conteúdo/TheTeacherDisappearence/items/keys.py
@@ -6,8 +6,13 @@ inventario = Inventario()
 
 class Item(pygame.sprite.Sprite):
     
-    def __init__(self):
+    def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
+        self.caminho = os.path.join(os.path.dirname(__file__),"..", "data", "images", "items", "key.png")
+        self.image = pygame.image.load(self.caminho)
+        self.image = pygame.transform.scale(self.image, (25, 40))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = x, y
         self.coletado = False
         
     def update(self):
@@ -18,20 +23,14 @@ class Item(pygame.sprite.Sprite):
             
 class Key1(Item):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(x, y)
         self.caminho = os.path.join(os.path.dirname(__file__),"..", "data", "images", "items", "key.png")
-        self.image = pygame.image.load(self.caminho)
-        self.image = pygame.transform.scale(self.image, (25, 40))
-        self.rect = self.image.get_rect()
-        self.rect.topleft = 500, 520
+        
 
 class Fita(Item):
     
-    def __init__(self):
-        super().__init__()
+    def __init__(self, x, y):
+        super().__init__(x, y)
         self.caminho = os.path.join(os.path.dirname(__file__),"..", "data", "images", "items", "Itens_do_jogador.png")
-        self.image = pygame.image.load(self.caminho)
-        self.image = pygame.transform.scale(self.image, (25, 40))
-        self.rect = self.image.get_rect()
-        self.rect.topleft = 500, 520
+        
