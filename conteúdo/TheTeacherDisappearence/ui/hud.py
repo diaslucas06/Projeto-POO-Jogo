1302e5b0eca52ac965aabe0673074a4f2f3a76c7
Lucas Gabriel de Araújo Dias <dias.lucas1@escolar.ifrn.edu.br>
2025-10-17 11:14:23 -0300

    Criação da classe Hud para para manter os textos que aparecem ao colidir com itens, portas ou personagens.

--- conteúdo/TheTeacherDisappearence/ui/hud.py
+++ conteúdo/TheTeacherDisappearence/ui/hud.py
@@ -4,6 +4,9 @@ import os
 INVENTARIO_ALTURA = 90
 INVENTARIO_LARGURA = 700
 
+WHITE = (255, 255, 255)
+
+
 class Inventario():
 
     def __init__(self):
@@ -11,3 +14,18 @@ class Inventario():
         self.image = pygame.transform.scale(self.image, (INVENTARIO_LARGURA, INVENTARIO_ALTURA))
         self.items = []
         self.i = 280
+        
+class Hud():
+    
+    def __init__(self):
+        self.font = pygame.font.Font(os.path.join(os.path.dirname(__file__), "..", "data", "fonts", "Minecraftia-Regular.ttf"), 20)
+        self.entrar = self.font.render("Pressione 'E' para entrar na porta", True, WHITE)
+        self.interagir = self.font.render("Pressione 'I' para interagir com o personagem", True, WHITE)
+        self.pegar = self.font.render("Pressione 'P' para pegar o item", True, WHITE)
+        self.tecla_p = pygame.image.load(os.path.join(os.path.dirname(__file__), "..", "data", "images", "teclas", "tecla_p.png"))
+        self.tecla_p = pygame.transform.scale(self.tecla_p, (30, 30))
+        self.tecla_i = pygame.image.load(os.path.join(os.path.dirname(__file__), "..", "data", "images", "teclas", "tecla_i.png"))
+        self.tecla_i = pygame.transform.scale(self.tecla_i, (30, 30))
+        self.tecla_e = pygame.image.load(os.path.join(os.path.dirname(__file__), "..", "data", "images", "teclas", "tecla_e.png"))
+        self.tecla_e = pygame.transform.scale(self.tecla_e, (30, 30))
+        
