53941faf781ddfeca44be6c2e2b1eed5f4ec7da6
Lucas Gabriel de Araújo Dias <dias.lucas1@escolar.ifrn.edu.br>
2025-10-17 11:14:46 -0300

    Criação de classes para os personagens.

--- conteúdo/TheTeacherDisappearence/characters/base.py
+++ conteúdo/TheTeacherDisappearence/characters/base.py
@@ -1,1 +1,20 @@
- 
+import pygame 
+import os
+
+class Base_Personagem(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(self.caminho)
+        self.image = pygame.transform.scale(self.image, (180, 350))
+        self.rect = self.image.get_rect()
+        
+    def falar(self):
+        return None
+    
+class Hugo(Base_Personagem):
+    
+    def __init__(self):
+        self.caminho = os.path.join(os.path.dirname(__file__),"..", "data", "images", "personagens", "hugo_idle.png")
+        super().__init__()
+        
