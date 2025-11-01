51da0d63540237f22030ad906d261565b2310772
EmillyMirely <mariz.e@escolar.ifrn.edu.br>
2025-11-01 18:58:53 -0300

    Criação do primeiro dialogo

--- conteúdo/TheTeacherDisappearence/characters/dialogue.py
+++ conteúdo/TheTeacherDisappearence/characters/dialogue.py
@@ -1,1 +1,20 @@
- 
+ #Primeiro Diálogo da Personagem com Hugo.
+
+#Aluna:
+ #“O senhor parece assustado. O que realmente está acontecendo? Ela desapareceu mesmo dentro do campus?”
+#Hugo:
+ #“Sim, ou pelo menos é o que dizem. A última vez que a vi foi... deixa eu lembrar... quinta-feira, por volta das sete da noite. Ela saiu apressada, com alguns arquivos no pendrive. Depois disso, sumiu.”
+#Aluna:
+ #“Sete da noite... quinta-feira... isso foi antes da queda de energia, não foi? Isso tem alguma ligação?”
+#Hugo:
+ #“Talvez tenha. Mas eu não posso dizer mais nada... é perigoso. Já falei demais.”
+#Aluna:
+ #“Se eu não entender o que está acontecendo, ninguém vai. Eu preciso de alguma pista.”
+#Hugo:
+ #“Certo, se quer respostas, vai ter que provar que pode lidar com elas. Vá até o Laboratório M6. Há um sistema antigo lá, algo que só quem decifra é quem é digno de alguma pista.”
+#Hugo:
+ #“Lá dentro há um terminal de segurança. Se conseguir resolver o código travado nele... eu te direi o resto. Só tenha cuidado!
+#Aluna:
+ #“Entendido. Vou até o M6 e volto assim que decifrar o código.”
+#Hugo:
+ #“Rápido. E... não diga a ninguém que conversou comigo. Eu não quero ser o próximo a desaparecer.”
