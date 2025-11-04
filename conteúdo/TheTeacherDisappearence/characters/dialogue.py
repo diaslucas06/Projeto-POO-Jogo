4fe0f6d0bf4b91c5586a7f79670f463497ff7850
EmillyMirely <mariz.e@escolar.ifrn.edu.br>
2025-11-03 21:13:57 -0300

    Continuação do diálogo com Hugo

--- conteúdo/TheTeacherDisappearence/characters/dialogue.py
+++ conteúdo/TheTeacherDisappearence/characters/dialogue.py
@@ -1,4 +1,4 @@
- #Primeiro Diálogo da Personagem com Hugo.
+#Primeiro Diálogo da Personagem com Hugo:
 
 #Aluna:
  #“O senhor parece assustado. O que realmente está acontecendo? Ela desapareceu mesmo dentro do campus?”
@@ -18,3 +18,44 @@
  #“Entendido. Vou até o M6 e volto assim que decifrar o código.”
 #Hugo:
  #“Rápido. E... não diga a ninguém que conversou comigo. Eu não quero ser o próximo a desaparecer.”
+
+
+#Seguna Parte do Diálogo com Hugo:
+
+#Hugo:
+#Você voltou! Isso é... impressionante. Confesso que achei que você não fosse conseguir.
+
+#ALuna:
+#Consegui. O código estava escondido entre vários arquivos antigos. Parecia uma armadilha, mas eu consegui acessar o terminal.
+
+#ALuna:
+#Agora o senhor pode cumprir a promessa. O que mais sabe sobre o que aconteceu com a professora Maíra?
+
+#Hugo:
+#Maíra estava investigando algo grande, algo que envolvia o próprio sistema interno da escola. Ela guardou informações confidenciais, e me deixou responsável por uma pista. 
+#Eu tenho esse pendrive que é uma parte do quebra-cabeça.
+
+#ALuna:
+#O que tem dentro dele?
+
+#Hugo:
+#Eu não sei. Só sei que Maíra me pediu pra mantê-lo seguro caso algo acontecesse com ela. E me disse que só alguém de confiança e que revolvesse o problema no M6, poderia ter acesso a ele.
+
+#Aluna:
+#Obrigada Hugo. Vou fazer o possível para descobrir o que está acontecendo. 
+
+#Hugo:
+#Espere..
+#Devido o que aconteceu, todos os computadores estão bloqueados, então somente um vai te dar a resposta que vc quer, e ele não é fácil de acessar...
+
+#ALuna:
+#E como vou saber qual é esse computador?
+
+#Hugo:
+#Maíra deixou pistas espalhadas, lembra? Uma delas deve estar na sala A38. Ela passava horas lá à noite, antes de tudo acontecer. Pegue esta chave.
+
+#Narração:
+#Item obtido: Chave da sala A38.
+
+#Hugo:
+#Agora vá, antes que alguém perceba que voltou. E lembre-se, nem tudo aqui é o que parece. 
